576e83a78fa8a08a4f84acdf0b91379389af9404
Maemolee <iblueer@live.cn>
2018-01-08 15:27:40 +0800

    Initial Commit

---
+++ pathProcessor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pathProcessor
+//
+//  Created by 宅音かがや on 2018/01/08.
+//  Copyright © 2018年 宅音かがや. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
